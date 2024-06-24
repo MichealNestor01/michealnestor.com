0c7447c1953958e26a49237463a01b674a369cfc
Micheal Nestor <michealnestor@outlook.com>
2024-06-24 17:41:10 +0100

    removed container div

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,7 @@
-import { toggleSiteTheme } from "../functions/toggleSiteTheme";
-
 export default function Landing() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline text-primary-colour">
-        Micheal Nestor - light mode dark mode template
-      </h1>
-      <button className="text-primary-colour" onClick={() => toggleSiteTheme()}>
-        Change Theme
-      </button>
-    </div>
+    <>
+      <h1 className="text-l text-primary-colour">landing page</h1>
+    </>
   );
 }
