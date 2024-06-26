eb4754075387ef243e7c5c36d6e575531ec9d532
Micheal Nestor <michealnestor@outlook.com>
2024-06-26 12:08:32 +0100

    added delay to click

--- src/components/ToggleButton/ToggleButton.tsx
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { flyInOutY } from "../../framer-presets/animation-presets";
 
 type ToggleButtonProps = {
@@ -16,9 +17,23 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({
   optionA,
   optionB,
 }) => {
+  const [enableButton, setEnableButton] = useState<boolean>(true);
   const classes = `${className} overflow-y-hidden`;
+
+  const clickHandler = () => {
+    // This is needed because animate presents does not behave well with fast clicks
+    // so delay presses to be 1.1 times the duration of the animation
+    if (enableButton) {
+      setEnableButton(false);
+      onClick();
+      setTimeout(() => {
+        setEnableButton(true);
+      }, flyInOutY.transition.duration * 1100);
+    }
+  };
+
   return (
-    <button onClick={onClick} className={classes}>
+    <button onClick={clickHandler} className={classes}>
       <AnimatePresence initial={false} mode="wait">
         {optionAactive ? (
           <motion.div key="optionA" {...flyInOutY}>
