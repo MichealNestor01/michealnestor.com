631134d3f9f614696d614189beb0c5ad39dd8651
Micheal Nestor <michealnestor@outlook.com>
2024-06-27 17:17:46 +0100

    added card to landing page to test functionality

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,19 @@
+import Card from "../components/Cards/EmploymentCard";
+
 export default function Landing() {
   return (
-    <>
+    <section>
       <h1 className="text-l text-primary-colour">landing page</h1>
-    </>
+      <div className="w-1/2 h-96">
+        <Card
+          company="Bank of America"
+          role="Summer Technology Analyst"
+          startDate="20/06/23"
+          endDate="28/08/23"
+          description="I wasn an intern working in the Equities Linked  Technology Team. I worked on an automated report that identifies inefficiencies in the intra-day risk engine, delivers insights on how to address these inefficiencies and recommends how work should be redistributed across the grid."
+          skills={["Python", "Scaled Agile", "Elastic Search", "Data Science"]}
+        />
+      </div>
+    </section>
   );
 }
