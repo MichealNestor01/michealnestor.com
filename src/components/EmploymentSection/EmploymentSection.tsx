558f6c6a82f41a0a69aebb782cc890ad63df3c2f
Micheal Nestor <michealnestor@outlook.com>
2024-12-08 16:07:54 +0000

    moved employment data to a json file

--- src/components/EmploymentSection/EmploymentSection.tsx
+++ src/components/EmploymentSection/EmploymentSection.tsx
@@ -1,5 +1,6 @@
 import SkillsSpinner from "./SkillsSpinner";
 import EmploymentCard from "../Cards/EmploymentCard";
+import workExperienceData from "../../data/workExperience.json";
 
 export default function EmploymentSection() {
   return (
@@ -10,66 +11,21 @@ export default function EmploymentSection() {
           <h1>EXPERIENCE</h1>
         </div>
         <SkillsSpinner />
-        <div className="mb-10" />
       </div>
       <div className="relative row-start-1 col-start-1 flex flex-row justify-end w-full h-fit">
         <div className="w-1/2 flex flex-col items-end">
-          <div className="mb-24" />
-          <EmploymentCard
-            company="Bank of America"
-            role="Technology Analyst"
-            startDate="10/07/24"
-            endDate="Present"
-            description="Software Engineer in the Equities Linked Technology team."
-            skills={[
-              "Python",
-              "Scaled Agile",
-              "Elastic Search",
-              "Information Visualisation",
-            ]}
-          />
-          <div className="mb-20" />
-          <EmploymentCard
-            company="Bank of America"
-            role="Summer Technology Analyst"
-            startDate="20/06/23"
-            endDate="28/08/23"
-            description="I wasn an intern working in the Equities Linked  Technology Team. I worked on an automated report that identifies inefficiencies in the intra-day risk engine, delivers insights on how to address these inefficiencies and recommends how work should be redistributed across the grid."
-            skills={[
-              "Python",
-              "Scaled Agile",
-              "Elastic Search",
-              "Data Science",
-            ]}
-          />
-          <div className="mb-20" />
-          <EmploymentCard
-            company="BAE Systems DI"
-            role="Software Engineer Intern"
-            startDate="20/06/22"
-            endDate="20/09/22"
-            description="I was placed in the national security buisness  area, and worked closely with law enforcement agencies to research and  create new software for them."
-            skills={[
-              "Python",
-              "Machine Learning",
-              "Web Scraping",
-              "Containerisation",
-            ]}
-          />
-          <div className="mb-20" />
-          <EmploymentCard
-            company="University of Leeds"
-            role="Student Amabassador"
-            startDate="01/08/21"
-            endDate="01/07/24"
-            description="Student ambassador for Computer Science for my year, worke closely with department leadership, module leaders and the student support office to ensure that any issues facing my peers were brought to light and addressed promptly and effectively."
-            skills={[
-              "Leadership",
-              "Arbitration",
-              "Negotiation",
-              "Professional Communication",
-            ]}
-          />
+          {workExperienceData.map((experience, index) => (
+            <div key={index} className="mb-24">
+              <EmploymentCard
+                company={experience.company}
+                role={experience.role}
+                startDate={experience.startDate}
+                endDate={experience.endDate}
+                description={experience.description}
+                skills={experience.skills}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
