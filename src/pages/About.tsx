eaf88cdb4360c500e7e004d3355cd15ea287c7c1
Micheal Nestor <michealnestor@outlook.com>
2025-01-22 21:01:11 +0000

    Started on about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,14 @@
 export default function About() {
   return (
-    <section className="">
-      <h1 className="text-3xl font-bold underline main-colour">About Page</h1>
+    <section className="bg-red-500 w-full flex items-start">
+      <div className="w-1/5" id="navigator"></div>
+
+      <div className="bg-blue-500 w-4/5 flex justify-between">
+        <h1 className="text-3xl font-bold underline main-colour">
+          Professional Experience
+        </h1>
+        <h1 className="text-3xl font-bold underline main-colour">About Me</h1>
+      </div>
     </section>
   );
 }
